8958621f268e83f7bd1d2b74a08347c91006de1b
FRC Team Blitz #2083 <github@teamblitz.net>
2018-03-17 13:26:40 -0600

    More code clean up.

--- src/org/usfirst/frc/team2083/subsytems/ArmSubsystem.java
+++ src/org/usfirst/frc/team2083/subsytems/ArmSubsystem.java
@@ -9,7 +9,8 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class ArmSubsystem extends Subsystem{
+public class ArmSubsystem extends Subsystem {
+	
 	public double motorSpeed;
 	public double stallSpeed;
 	WPI_TalonSRX armMotor;
@@ -18,8 +19,7 @@ public class ArmSubsystem extends Subsystem{
 	DigitalInput top;
 	DigitalInput bottom;
 	
-	
-	 public ArmSubsystem() {
+	public ArmSubsystem() {
 		 super();
 	    top = RobotMap.ArmPositionUpper;
 	    bottom = RobotMap.ArmPositionLower;
@@ -30,8 +30,8 @@ public class ArmSubsystem extends Subsystem{
 	 	
 	 	System.out.println("top: " + top);
 	 	System.out.println("bottom:" + bottom);
-	    }
-//	    
+	 }
+  
 //	 public void armSetter() {
 //	    top = RobotMap.ArmPositionUpper;
 //	    bottom = RobotMap.ArmPositionLower;
@@ -40,64 +40,63 @@ public class ArmSubsystem extends Subsystem{
 //	 	System.out.println("A very very very long string so you can't miss it.");
 //	    }
 
-	  	
-	    public void moveUp()
-		{
-//	    	armSetter();
-	    	if(top.get()) {
-	    		armSlave.follow(armMotor);
-	    		armMotor.set(ControlMode.PercentOutput, motorSpeed);
-	    	}
-	    	else {
-	    		zeroOut();
-	    	}
-		}
-	    public void moveDown() 
-	    {
-//	    	armSetter();
-	    	if(bottom.get()) {
-	    		armSlave.follow(armMotor);
-	    		armMotor.set(ControlMode.PercentOutput, -motorSpeed);
-	    	}
-	    	else {
-	    		zeroOut();
-	    	}
-	    }
-	    public void hold() 
-	    {
-//	    	armSetter();
-	    	if(bottom.get()) {
-	    		armSlave.follow(armMotor);
-	    		armMotor.set(ControlMode.PercentOutput, stallSpeed);
-	    	}
-	    	else {
-	    		zeroOut();
-	    	}
-	    }
-	    public void zeroOut() 
-	    {
-//	    	armSetter();
-	    		armSlave.set(ControlMode.PercentOutput, 0);
-	    		armMotor.set(ControlMode.PercentOutput, 0);
-	    }
-	    
-	    
-	    
-	    protected void initDefaultCommand()
-	    {
-	    	System.out.println("Arm 'Initiate Default Command'");
-	    }
-	    
-	    public void enableControl()
-	    {
-	    	armMotor.set(ControlMode.PercentOutput, 0);
-	    	System.out.println("Arm Control Enabled");
-	    }
+    public void moveUp()
+	{
+//	    armSetter();
+    	if(top.get()) {
+    		armSlave.follow(armMotor);
+    		armMotor.set(ControlMode.PercentOutput, motorSpeed);
+    	}
+    	else {
+    		zeroOut();
+    	}
+	}
+    
+    public void moveDown() 
+    {
+//	    armSetter();
+    	if(bottom.get()) {
+    		armSlave.follow(armMotor);
+    		armMotor.set(ControlMode.PercentOutput, -motorSpeed);
+    	}
+    	else {
+    		zeroOut();
+    	}
+    }
+    
+    public void hold() 
+    {
+//	    armSetter();
+    	if(bottom.get()) {
+    		armSlave.follow(armMotor);
+    		armMotor.set(ControlMode.PercentOutput, stallSpeed);
+    	}
+    	else {
+    		zeroOut();
+    	}
+    }
+    
+    public void zeroOut() 
+    {
+//	    armSetter();
+		armSlave.set(ControlMode.PercentOutput, 0);
+		armMotor.set(ControlMode.PercentOutput, 0);
+    }
+    
+    protected void initDefaultCommand()
+    {
+    	System.out.println("Arm 'Initiate Default Command'");
+    }
+    
+    public void enableControl()
+    {
+    	armMotor.set(ControlMode.PercentOutput, 0);
+    	System.out.println("Arm Control Enabled");
+    }
 
-	    public void disableControl()
-	    {
-	    	System.out.println("Arm Control Disabled");
-	    	armMotor.disable();
-
-	    }
+    public void disableControl()
+    {
+    	System.out.println("Arm Control Disabled");
+    	armMotor.disable();
+    }
 }
