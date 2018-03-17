bcab14084be47e0161909f15612e70a5efb5069e
FRC Team Blitz #2083 <github@teamblitz.net>
2018-03-17 13:38:41 -0600

    Minor tweak.

--- src/org/usfirst/frc/team2083/subsytems/GripperSubsystem.java
+++ src/org/usfirst/frc/team2083/subsytems/GripperSubsystem.java
@@ -6,7 +6,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-
 public class GripperSubsystem extends Subsystem {
 	
 	double motorSpeed = 0.5;
